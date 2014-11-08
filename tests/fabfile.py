0c8b3e57c20115fbeadbe11f24784088b8c94374
cts12 <charchris@race-nation.com>
2014-11-08 19:59:14 +0000

    changed function name #1

--- tests/fabfile.py
+++ tests/fabfile.py
@@ -6,7 +6,7 @@ import os
 env.hosts = ['guest@cvm-g1436217.doc.ic.ac.uk']
 env.port = '55022'
 
-def get_video():
+def get_videos():
   env.password = getpass.getpass('Password: ')
   run('mkdir -p /home/guest/videos/')
   videos = get('/home/guest/videos/', '%(dirname)s/%(basename)s')
