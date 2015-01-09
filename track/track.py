25d24e6e812f6c3f039c3ff33dc09d3d32268d52
Edwin K <edwink@ubuntu.(none)>
2015-01-09 16:16:38 +0000

    Proximity and prediction behid obstruction added. Works for some cases

--- track/track.py
+++ track/track.py
@@ -43,6 +43,7 @@ def JSON_from_video(source):
     wait_per_frame = int(1000 / int(cap.get(cv2.cv.CV_CAP_PROP_FPS)))
     back_sub = _setup_background_subtractor()
     frame_no = 0
+    threshold = 100
     frames = []
 
     # Set up the kalman filter
@@ -65,21 +66,41 @@ def JSON_from_video(source):
 
     # These will get the trajectories for mouse location and Kalman estiamte
     kalman_points = []
+    # set kalman transition matrix
+    kalman.transition_matrix[0, 0] = 1
+    kalman.transition_matrix[0, 1] = 0
+    kalman.transition_matrix[0, 2] = 1
+    kalman.transition_matrix[0, 3] = 0
+    kalman.transition_matrix[1, 0] = 0
+    kalman.transition_matrix[1, 1] = 1
+    kalman.transition_matrix[1, 2] = 0
+    kalman.transition_matrix[1, 3] = 0
+    kalman.transition_matrix[2, 0] = 0
+    kalman.transition_matrix[2, 1] = 0
+    kalman.transition_matrix[2, 2] = 0
+    kalman.transition_matrix[2, 3] = 1
+    kalman.transition_matrix[3, 0] = 0
+    kalman.transition_matrix[3, 1] = 0
+    kalman.transition_matrix[3, 2] = 0
+    kalman.transition_matrix[3, 3] = 1
+    prev_x = None
 
     while(True):
+        found = False
         ret, frame = cap.read()
 
         if not ret:
             break
-
+        
+        cv2.rectangle(frame, (400,0), (454, 480), (0, 255, 255), -1)
         frameblur = cv2.blur(frame, (5, 5))
         fgmask = back_sub.apply(frameblur, learningRate=0.001)
-
+        #cv2.imshow('bg', fgmask)
         contours, hier = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,
                                           cv2.CHAIN_APPROX_SIMPLE)
 
         bounds = map(cv2.boundingRect, contours)
-
+       
         if bounds:
             frame_data = {}
             frame_data['frame'] = frame_no
@@ -87,53 +108,42 @@ def JSON_from_video(source):
 
             measured = (bx + bw / 2, by + bh / 2)
 
-            # set kalman transition matrix
-            kalman.transition_matrix[0, 0] = 1
-            kalman.transition_matrix[0, 1] = 0
-            kalman.transition_matrix[0, 2] = 1
-            kalman.transition_matrix[0, 3] = 0
-            kalman.transition_matrix[1, 0] = 0
-            kalman.transition_matrix[1, 1] = 1
-            kalman.transition_matrix[1, 2] = 0
-            kalman.transition_matrix[1, 3] = 0
-            kalman.transition_matrix[2, 0] = 0
-            kalman.transition_matrix[2, 1] = 0
-            kalman.transition_matrix[2, 2] = 0
-            kalman.transition_matrix[2, 3] = 1
-            kalman.transition_matrix[3, 0] = 0
-            kalman.transition_matrix[3, 1] = 0
-            kalman.transition_matrix[3, 2] = 0
-            kalman.transition_matrix[3, 3] = 1
-            # If i have found something vaguely human
-            if 3 * bw < bh:
-                # if found is False:
-                found = True
-
-                cv2.rectangle(frame, (bx, by), ((bx + bw), (by + bh)),
-                              (0, 255, 0), 3)
-                frame_data['rectangles'] = [{'id': '',
+           # If i have found something vaguely human
+            if 2 * bw < bh:
+               curr_x = bx + bw / 2
+               if prev_x is None or abs(curr_x - prev_x) < threshold:
+                 found = True
+                 cv2.rectangle(frame, (bx, by), ((bx + bw), (by + bh)),
+                               (0, 255, 0), 3)
+                 frame_data['rectangles'] = [{'id': '',
                                              'x': bx,
                                              'y': by,
                                              'width': bw,
                                              'height': bh}]
-                frames.append(frame_data)
+                 frames.append(frame_data)
             # I only update the value if it is correct and has been found.
+            predicted = cv.KalmanPredict(kalman)
+            drawCross(frame, (predicted[0, 0], predicted[1, 0]), 255, 0, 0)
             if found is True:
                 # Update the Kalman filter with these mesaurements
                 kalman_measurement[0, 0] = bx + bw / 2
                 kalman_measurement[1, 0] = by + bh / 2
                 # Predict and update the internals of the kalman
-                cv.KalmanPredict(kalman)
+                
                 # Corrrect the estimate
                 corrected = cv.KalmanCorrect(kalman, kalman_measurement)
                 estimated = (corrected[0, 0], corrected[1, 0])
+                prev_x = predicted[0, 0]
                 kalman_points.append(estimated)
+                print 'predicted = ' + str(predicted[0,0])
+                print 'corrected = ' + str(corrected[0,0])
+                print 'measured  = ' + str(bx + bw / 2)
 
                 # Display the trajectories and current points
-                drawLines(frame, kalman_points, 0, 255, 0)
-                drawCross(frame, estimated, 255, 255, 255)
-                drawCross(frame, measured, 0, 0, 255)
-
+                #drawLines(frame, kalman_points, 0, 255, 0)
+                #drawCross(frame, estimated, 255, 255, 255)
+                #drawCross(frame, measured, 0, 0, 255)
+        
         cv2.imshow('frame', frame)
         key = cv2.waitKey(wait_per_frame) & 0xff
         if key == 27:
